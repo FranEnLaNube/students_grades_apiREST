b508e31f0a82f97910d28dafda4f20fd07b2540a
Francisco <ciscoSolana@gmail.com>
2024-11-21 23:39:25 +0100

    feature: edit Grade model to have binary primary key

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -2,25 +2,59 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Student;
+use App\Models\Subject;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Grade extends Model
 {
-    /** @use HasFactory<\Database\Factories\GradeFactory> */
     use HasFactory;
-    use HasUuids;
 
     protected $fillable = [
         'student_id',
         'subject_id',
         'grade',
     ];
+
+    public $incrementing = false;
+
+    protected $keyType = null;
+
+    protected $primaryKey = null;
+
+    protected function setKeysForSaveQuery($query)
+    {
+        $query->where('student_id', $this->getAttribute('student_id'))
+              ->where('subject_id', $this->getAttribute('subject_id'));
+
+        return $query;
+    }
+
+    public function getKeyName()
+    {
+        return ['student_id', 'subject_id'];
+    }
+
+    public function getKeyType()
+    {
+        return null;
+    }
+
+    public function getKey()
+    {
+        return null;
+    }
+    public function is($model)
+    {
+        return $this->student_id === $model->student_id
+            && $this->subject_id === $model->subject_id;
+    }
     public function student()
     {
         return $this->belongsTo(Student::class);
     }
+
     public function subject()
     {
         return $this->belongsTo(Subject::class);
--- database/migrations/2024_11_20_090014_create_grades_table.php
+++ database/migrations/2024_11_20_090014_create_grades_table.php
@@ -5,25 +5,18 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
         Schema::create('grades', function (Blueprint $table) {
-            $table->uuid('id')->primary();
             $table->foreignUuid('student_id')->constrained('students')->onDelete('cascade');
             $table->foreignUuid('subject_id')->constrained('subjects')->onDelete('cascade');
-            $table->decimal('grade', 5, 2);
+            $table->float('grade');
             $table->timestamps();
 
-            $table->unique(['student_id', 'subject_id']);
+            $table->primary(['student_id', 'subject_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('grades');
